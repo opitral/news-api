4e261b5d08d85271ce73d5e32a2b20080a6a0861
Volodymyr Slobodian <opitral@proton.me>
2024-11-08 23:24:53 +0200

    fix validations

--- validations/NewsValidation.js
+++ validations/NewsValidation.js
@@ -27,6 +27,37 @@ export const newsCreateValidator = [
         })
 ]
 
+export const newsUpdateValidator = [
+    body("id", "Id must be a valid ObjectId")
+        .isMongoId(),
+    body("title", "Title must be from 8 to 128 characters")
+        .optional()
+        .isLength({
+            min: 8,
+            max: 128
+        }),
+    body("content", "Content must be from 8 to 1024 characters")
+        .optional()
+        .isLength({
+            min: 8,
+            max: 1024
+        }),
+    body("image", "Image url must be from 8 to 256 characters")
+        .optional()
+        .isURL()
+        .isLength({
+            min: 8,
+            max: 256
+        }),
+    body("video", "Video url must be from 8 to 256 characters")
+        .optional()
+        .isURL()
+        .isLength({
+            min: 8,
+            max: 256
+        })
+]
+
 export const commentCreateValidator = [
     body("content", "Content must be from 8 to 1024 characters")
         .isLength({
