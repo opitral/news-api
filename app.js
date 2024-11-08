4e1d6605a1246d527802b4d8e2e7206a81119d54
Volodymyr Slobodian <opitral@proton.me>
2024-11-08 23:25:42 +0200

    connect controller and rename starter file

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv';
 
+import NewsController from './controllers/NewsController.js';
+import UserService from "./services/UserService.js";
+
 dotenv.config();
+const userService = new UserService();
 
 mongoose.connect(process.env.DB_URL)
     .then(() => {
@@ -19,6 +23,18 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(async (req, _, next) => {
+    try {
+        await userService.getUserByIpElseCreate(req.ip);
+        await userService.updateUserLastOnline(req.ip);
+    } catch (error) {
+        console.error(`Error in user IP handling middleware: ${error}`);
+    }
+    next();
+});
+
+app.use("/api/news", NewsController);
 
 app.listen(port, (error) => {
     if (error) {
